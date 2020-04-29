51ec5b93a1b6d7f3217ecbd888e27d2b3d6f6f5a
Komal <K.jha@castsoftware.com>
2020-04-29 22:00:40 +0530

    Add first route, eslint and webpack for production

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,21 @@
 import React, {useState} from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 
 export default function Form() {
     const [count, setCount] = useState(0);
 
-    return(<div>
+    return[<div>
         <p>You clicked the button {count} times</p>
         <button onClick={() => setCount(count+1)}>Clicked</button>
-    </div>)
+    </div>]
 }
 
 const wrapper = document.getElementById("container");
-wrapper ? ReactDOM.render(<Form />, wrapper) : false;
+wrapper ? ReactDOM.render(<Router>
+    <Switch>
+        <Route exact={true} path={'/welcome'} component={Form}/>
+        <Route render={ () => <Redirect to={'/welcome'}/>} />
+    </Switch>
+
+</Router>, wrapper) : false;
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './build',
+    },
     module: {
         rules: [
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
+                use: ['babel-loader']
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve('./src/index.html'),
+        }),
+    ]
 };
